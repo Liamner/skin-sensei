bb5ffd2cd2d3a2ecb04ee8321f6a7eb8af393283
Liamner <nuria.perez.olivares@gmail.com>
2023-05-07 20:17:17 +0200

    fix: :bug: fix eslint

--- app/api/users/route.js
+++ app/api/users/route.js
@@ -1,6 +1,6 @@
 import { clientPromise } from 'lib/mongodb';
 
-export async function GET(request) {
+export async function GET() {
   try {
     const client = await clientPromise;
     const db = client.db(process.env.DATABASE);
@@ -13,7 +13,7 @@ export async function GET(request) {
   }
 }
 
-export async function DELETE(request) {
+export async function DELETE() {
   try {
     const client = await clientPromise;
     const db = client.db(process.env.DATABASE);
